732f15ad96a0010147ad960a60457d30b6163a63
julia <me@juliapaci.com>
2025-12-09 14:52:49 +1100

    qutebrowser yay

---
+++ config/qutebrowser/config.py
@@ -0,0 +1,10 @@
+config.load_autoconfig()
+
+c.content.javascript.enabled = False
+c.content.images = False
+
+c.tabs.show = "switching"
+c.statusbar.show = "never"
+
+c.scrolling.smooth = True
+c.scrolling.bar = "never"
